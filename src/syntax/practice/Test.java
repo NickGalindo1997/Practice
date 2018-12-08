4f39517f3459e96dcced671640c471a240e71dfe
NickGalindo1997 <Nick.mg1997@gmail.com>
2018-12-08 12:22:55 -0500

    my commit

---
+++ src/syntax/practice/Test.java
@@ -0,0 +1,11 @@
+package syntax.practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello from test");
+		System.out.println("I am making changes");
+	}
+
+}
